49d685004a16783ec67a12cc970bdce7c76607a1
Dtard <barrierloss@yahoo.com>
2018-12-09 16:04:11 -0800

    Add Webext proj
    New cat & proj

---
+++ Web-Ext/App-D-Stroyer/ToolsAPIs.js
@@ -0,0 +1,7 @@
+/*  APIs from <developer.chrome.com/extensions/api_index> that may be useful 
+    (API Usage: "chrome.apiName")
+    STABLE APIs [-NAME | DESCRIPTION]:
+    -bookmarks | Create, organize, and manipulate bookmarks
+    -browserAction | put icons in the main Google Chrome toolbar, to the right of the address bar. In addition to its icon, a browser action can have a tooltip, a badge, and a popup.
+    
+*/
